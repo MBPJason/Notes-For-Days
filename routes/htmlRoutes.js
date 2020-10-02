07d1ac4cd76b868eb20107891d710ff053b5b400
Jason Ozulumba <cjozulumba@gmail.com>
2020-10-02 17:07:37 -0400

    Built first skeleton part out, getting the html pages and displaying it

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,1 +1,22 @@
-const path  = require("path");
+const path = require("path");
+
+const index = path.join(__dirname, "public", "index.html");
+const notes = path.join(__dirname, "public", "notes.html");
+
+
+// HTML Routing
+
+module.exports = function (app) {
+  app.get("/", function (req, res) {
+    res.sendFile(index);
+  });
+
+  app.get("/notes", function(req, res) {
+      res.sendFile(notes);
+  });
+
+// Default to home, if no matching routes
+  app.get("*", function(req, res) {
+      res.sendFile(index);
+  })
+};
