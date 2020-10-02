73e4057f47834ee01d102c0c8bd0854327cf24d9
Jason Ozulumba <cjozulumba@gmail.com>
2020-10-02 18:51:55 -0400

    Testing out server

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require("express");
-const fs = require("fs");
-
+// Making an express server
 const app = express();
 
-
+// Sets up a port to work with
 const PORT = process.env.PORT || 8080;
 
 // Middleware
@@ -11,56 +10,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-// Routes
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+// Routes, it gives a map for user response
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
-app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "public", "notes.html"));
-});
-
-app.get("/api/notes", function(req, res) {
-    return res.sendFile(path.join(__dirname, "./db/db.json"));
-});
-
-// Listener
+// Listener, starts the server
 app.listen(PORT, function () {
-  console.log("App listening on PORT: " + PORT);
-});
-
-app.post("/api/notes", function(req, res) {
-    try {
-        notesData = fs.readFileSync("./db/db.json", "utf8");
-        console.log(notesData);
-        notesData = JSON.parse(notesData);
-        req.body.id = notesData.length;
-        notesData.push(req.body);
-        notesData = JSON.stringify(notesData);
-        fs.writeFile("./db/db.json", notesData, "utf8", function (error) {
-            if (error) throw error;
-        });
-
-        res.json(JSON.parse(notesData));
-    } catch (error) {
-        console.log("Darn monkeys they left a mess of things");
-        console.log(error);
-    }
-});
-
-app.delete("/api/notes/:id", function(req, res) {
-    try {
-        notesData = fs.readFileSync("./db/db.json", "utf8");
-        notesData = JSON.parse(notesData);
-        notesData = (notesData).filter(function (note) {
-            return note.id != req.params.id
-        });
-        notesData = JSON.stringify(notesData);
-        fs.writeFile("./db/db.json", notesData, "utf8", function (error) {
-            if (error) throw error;
-        });
-    } catch (error) {
-        console.log("Darn monkeys they left a mess of things");
-        console.log(error);
-    }
-});
+    console.log("App listening on PORT: " + PORT);
+  });
